3f3f1e55f5c423a165a992a1853311f7d8b935e3
TSylv <sirsylvester8@gmail.com>
2025-11-12 23:44:22 -0600

    Added POST /orders/:id/products and tested

--- routes/orders.js
+++ routes/orders.js
@@ -75,4 +75,45 @@ router.get("/:id", requireUser, async (req, res, next) => {
   }
 });
 
+
+router.post("/:id/products", requireUser, async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const orderId = req.params.id;
+    const { product_id, quantity } = req.body;
+
+    const {
+      rows: [order],
+    } = await db.query(
+      `
+        SELECT id, user_id
+        FROM orders
+        WHERE id = $1
+        AND user_id = $2;
+      `,
+      [orderId, userId]
+    );
+
+    if (!order) {
+      return res.status(404).send({ message: "Order not found" });
+    }
+
+    const {
+  rows: [orderProduct],
+} = await db.query(
+  `
+        INSERT INTO orders_products (order_id, product_id, quantity)
+        VALUES ($1, $2, $3)
+        RETURNING order_id, product_id, quantity;
+      `,
+  [orderId, product_id, quantity]
+);
+
+
+    res.status(201).send(orderProduct);
+  } catch (err) {
+    next(err);
+  }
+});
+
 export default router;
