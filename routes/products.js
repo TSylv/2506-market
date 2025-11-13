96d147cef59624ff06d3345c25e8cc77bafd3ac2
TSylv <sirsylvester8@gmail.com>
2025-11-12 21:05:00 -0600

    Added GET /products/id/orders sends all order made by the user that include the product.

--- routes/products.js
+++ routes/products.js
@@ -39,4 +39,30 @@ router.get("/:id", async (req, res, next) => {
 });
 
 
+router.get("/:id/orders", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const result = await db.query(
+      `
+      SELECT 
+        o.id AS order_id,
+        o.date,
+        o.note,
+        o.user_id,
+        op.quantity
+      FROM orders o
+      JOIN orders_products op ON o.id = op.order_id
+      WHERE op.product_id = $1;
+      `,
+      [id]
+    );
+
+    res.send(result.rows);
+  } catch (err) {
+    next(err);
+  }
+});
+
+
 export default router;
